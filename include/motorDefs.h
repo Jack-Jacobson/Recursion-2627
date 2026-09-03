23cebfe9771b58ac2e3a8b87b51e9c01d38bc624
Jack Jacobson <jackjacobson.co@gmail.com>
2026-09-02 19:00:30 -0600

    init commit

---
+++ include/motorDefs.h
@@ -0,0 +1,9 @@
+#include "vex.h"
+
+extern vex::brain Brain;
+extern vex::motor frontLeftDrive;
+extern vex::motor midLeftDrive;
+extern vex::motor backLeftDrive;
+extern vex::motor frontRightDrive;
+extern vex::motor midRightDrive;
+extern vex::motor backRightDrive;
