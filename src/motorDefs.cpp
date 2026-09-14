702c9b4be11094b32e2eb6ea48c1bb45b18246d6
Jack Jacobson <jackjacobson.co@gmail.com>
2026-09-13 23:13:27 -0600

    update ports

--- src/motorDefs.cpp
+++ src/motorDefs.cpp
@@ -3,12 +3,12 @@
 
 vex::brain Brain;
 vex::controller Controller = vex::controller();
-vex::motor frontLeftDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
-vex::motor midLeftDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
-vex::motor backLeftDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
+vex::motor frontLeftDrive(vex::PORT9, vex::gearSetting::ratio6_1, false);
+vex::motor midLeftDrive(vex::PORT7, vex::gearSetting::ratio6_1, true);
+vex::motor backLeftDrive(vex::PORT10, vex::gearSetting::ratio6_1, false);
 vex::motor frontRightDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
-vex::motor midRightDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
-vex::motor backRightDrive(vex::PORT1, vex::gearSetting::ratio6_1, false);
+vex::motor midRightDrive(vex::PORT3, vex::gearSetting::ratio6_1, true);
+vex::motor backRightDrive(vex::PORT2, vex::gearSetting::ratio6_1, false);
 
-vex::rotation odomPod(vex::PORT4, false);
-vex::inertial inertialSensor(vex::PORT3);
+vex::rotation odomPod(vex::PORT14, false);
+vex::inertial inertialSensor(vex::PORT8);
